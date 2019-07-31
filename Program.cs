0f8d53a094d263fecf49babe9e2236b6fc27a544
Greg <gregsmith446@gmail.com>
2019-07-30 20:25:35 -0400

    completed!

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace almostIncreasingSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool almostIncreasingSequence(int[] sequence)
+            {
+
+                int length = sequence.Length;
+
+                // deal with 2 integer arrays  
+                if (length == 2)
+                {
+                    return true;
+                }
+
+                int countOne = 0;
+                int countTwo = 0;
+
+                // 1 for loop
+                for (int i = 0; i < length - 1; i++)
+                {
+                    // first if statement checks if there are numbers out of order and how many
+                    if (sequence[i] >= sequence[i + 1])
+                    {
+                        countOne++;
+                    }
+                    // second if statement checks if the numbers are in order 
+                    if (i != 0)
+                    {
+                        if (sequence[i - 1] >= sequence[i + 1])
+                        {
+                            countTwo++;
+                        }
+                    }
+                }
+
+                if (countOne == 1 && countTwo <= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            int[] testData = new int[] { 1, 3, 2, 1 };
+
+            var result = almostIncreasingSequence(testData);
+
+            Console.WriteLine(result);
+        }
+    }
+}
